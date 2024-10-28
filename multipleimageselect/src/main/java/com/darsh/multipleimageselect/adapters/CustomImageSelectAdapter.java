40d6a4f8d11864aeeb22116878b7483e6e5d85e6
Jesus Castro <castrojr913@gmail.com>
2024-10-28 17:42:43 -0500

    Solve java.lang.IndexOutOfBoundsException: Index 0 out of bounds for length 0

--- multipleimageselect/src/main/java/com/darsh/multipleimageselect/adapters/CustomImageSelectAdapter.java
+++ multipleimageselect/src/main/java/com/darsh/multipleimageselect/adapters/CustomImageSelectAdapter.java
@@ -3,11 +3,9 @@ package com.darsh.multipleimageselect.adapters;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 import com.darsh.multipleimageselect.R;
 import com.darsh.multipleimageselect.helpers.GlideHelper;
 import com.darsh.multipleimageselect.models.Image;
@@ -25,40 +23,32 @@ public class CustomImageSelectAdapter extends CustomGenericAdapter<Image> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder viewHolder;
-
         if (convertView == null) {
             convertView = layoutInflater.inflate(R.layout.grid_view_item_image_select, null);
-
             viewHolder = new ViewHolder();
-            viewHolder.imageView = (ImageView) convertView.findViewById(R.id.image_view_image_select);
+            viewHolder.imageView = convertView.findViewById(R.id.image_view_image_select);
             viewHolder.view = convertView.findViewById(R.id.view_alpha);
-
             convertView.setTag(viewHolder);
-
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
-
         viewHolder.imageView.getLayoutParams().width = size;
         viewHolder.imageView.getLayoutParams().height = size;
-
         viewHolder.view.getLayoutParams().width = size;
         viewHolder.view.getLayoutParams().height = size;
-
         if (position < arrayList.size() && arrayList.get(position).isSelected) {
             viewHolder.view.setAlpha(0.5f);
-            ((FrameLayout) convertView).setForeground(context.getResources().getDrawable(R.drawable.ic_done_white));
-
+            convertView.setForeground(context.getResources().getDrawable(R.drawable.ic_done_white));
         } else {
             viewHolder.view.setAlpha(0.0f);
-            ((FrameLayout) convertView).setForeground(null);
+            convertView.setForeground(null);
         }
-
-        Glide.with(context)
-                .setDefaultRequestOptions(GlideHelper.getDefaultRequestOptions())
-                .load(arrayList.get(position).path)
-                .into(viewHolder.imageView);
-
+        if (position < arrayList.size()) {
+            Glide.with(context)
+                    .setDefaultRequestOptions(GlideHelper.getDefaultRequestOptions())
+                    .load(arrayList.get(position).path)
+                    .into(viewHolder.imageView);
+        }
         return convertView;
     }
 
